d2c6c408e6696027194125bf836a3a46828c97d9
4kmalm <16521070@mahasiswa.itb.ac.id>
2023-02-24 14:06:19 +0700

    Setup Tucil 2

---
+++ src/main.py
@@ -0,0 +1,8 @@
+# Application to find nearest 2 point from n point in 3D space and calculate distance between them
+
+import math
+import os
+import randomizer as rand
+
+if __name__ == "__main__":
+    print("Hello Wordl!")
